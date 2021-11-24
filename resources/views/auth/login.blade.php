e90ceff08f66b05cca214bcc60d4b5c0c6d92266
linhchi2406 <linhchi24060508@gmail.com>
2021-11-24 10:10:19 +0700

    c

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,6 +42,9 @@
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                @endif
             </div>
         </form>
     </x-jet-authentication-card>
